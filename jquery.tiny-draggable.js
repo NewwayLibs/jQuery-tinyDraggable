87130e2a7a39a9f0b6589e8fc1b1c04a47811872
YAAP <yaapis@gmail.com>
2014-12-31 00:42:51 +0200

    callbacks

--- jquery.tiny-draggable.js
+++ jquery.tiny-draggable.js
@@ -1,24 +1,69 @@
 /*
-	jQuery tinyDraggable v1.0.2
-    Copyright (c) 2014 Simon Steinberger / Pixabay
-    GitHub: https://github.com/Pixabay/jQuery-tinyDraggable
-    More info: http://pixabay.com/blog/posts/p-52/
-	License: http://www.opensource.org/licenses/mit-license.php
-*/
+ jQuery tinyDraggable v1.0.3
+ Copyright (c) 2014 Simon Steinberger / Pixabay
+ GitHub: https://github.com/Pixabay/jQuery-tinyDraggable
+ More info: http://pixabay.com/blog/posts/p-52/
+ License: http://www.opensource.org/licenses/mit-license.php
+ */
 
-(function($){
-    $.fn.tinyDraggable = function(options){
-        var settings = $.extend({ handle: 0, exclude: 0 }, options);
-        return this.each(function(){
+(function ($) {
+
+    $.fn.tinyDraggable = function (options) {
+        var defaults = {
+            handle: 0,
+            exclude: 0,
+            allowXDrag:true,
+            allowYDrag:true,
+            start: null,
+            step: null,
+            finish: null
+        };
+        var settings = $.extend(defaults, options);
+
+        return this.each(function () {
             var dx, dy, el = $(this), handle = settings.handle ? $(settings.handle, el) : el;
             handle.on({
-                mousedown: function(e){
+                mousedown: function (e) {
+
                     if (settings.exclude && ~$.inArray(e.target, $(settings.exclude, el))) return;
+
                     e.preventDefault();
-                    var os = el.offset(); dx = e.pageX-os.left, dy = e.pageY-os.top;
-                    $(document).on('mousemove.drag', function(e){ el.offset({top: e.pageY-dy, left: e.pageX-dx}); });
+
+
+                    var os = el.offset();
+
+                    // start hook
+                    if (typeof( settings.start ) == 'function') {
+                        settings.start(el, os.top, os.left);
+                    }
+
+                    dx = e.pageX - os.left, dy = e.pageY - os.top;
+
+                    $(document).on('mousemove.drag', function (e) {
+
+                        var top = (settings.allowYDrag) ? (e.pageY - dy) : (os.top);
+                        var left = (settings.allowXDrag) ? (e.pageX - dx) : (os.left);
+
+                        el.offset({top: top, left: left});
+
+                        if (typeof( settings.step ) == 'function') {
+                            settings.step(el, top, left);
+                        }
+
+                    });
                 },
-                mouseup: function(e){ $(document).off('mousemove.drag'); }
+                mouseup  : function (e) {
+
+                    $(document).off('mousemove.drag');
+
+                    // stop hook
+                    if (typeof( settings.finish ) == 'function') {
+
+                        var os = el.offset();
+
+                        settings.finish(el, os.top, os.left);
+                    }
+                }
             });
         });
     }
